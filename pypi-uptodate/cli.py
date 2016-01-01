988934fae26ecb8478c5ae444ab46e932972ee02
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-01-01 22:21:01 +0000

    Print requirements

--- pypi-uptodate/cli.py
+++ pypi-uptodate/cli.py
@@ -3,14 +3,23 @@ import click
 
 
 @click.command()
-@click.option('--requirement', '-r', default='./requirements.txt', help='Specify the path of the requirements file.')
+@click.option('--requirement', '-r', default='requirements.txt', help='Specify the path of the requirements file.')
 def cli(requirement):
     """
     PyPI Up-to-date\n
     Check whether your PyPI requirements are up to date.
     """
-    click.echo("Hello World!")
-    pass
+
+    # click.echo("Hello World!")
+    click.echo("Opening: %s" % requirement)
+
+    try:
+        requirements = open(requirement).read().splitlines()
+        for line in requirements:
+            click.echo(line)
+
+    except OSError:
+        click.secho("Could not find %s. No such file or directory." % (requirement), fg='red')
 
 if __name__ == '__main__':
     cli()
