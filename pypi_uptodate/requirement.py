9c6e971ad905c3488b6d1c4cb3b5493a2ff9888f
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-01-02 02:26:17 +0000

    Skip non-pypi dependencies & Set Structure

--- pypi_uptodate/requirement.py
+++ pypi_uptodate/requirement.py
@@ -3,23 +3,35 @@ import re
 import requests
 
 
-class Requirement:
+class Requirement(object):
 
     def __init__(self, requirement):
-        self.setUp(requirement)
         self.name = None
         self.current_version = None
         self.latest_version = None
+        self.is_invalid = False
+        self.setUp(requirement)
 
     def setUp(self, requirement):
         regex = re.split("==", requirement)
-        self.name = regex[0]
-        self.current_version = regex[1]
-        self.get_package_info()
+        try:
+            self.name = regex[0]
+            self.current_version = regex[1]
+            self.get_package_info()
+        except IndexError:
+            self.is_invalid = True
 
     def get_package_info(self):
-        request = requests.get(('https://pypi.python.org/pypi/%s/json' % (self.name)))
-        response_json = request.json()
+        pypi_url = "https://pypi.python.org/pypi/%s/json" % (self.name)
+        request = requests.get(pypi_url)
+        if request.status_code == requests.codes.ok:
+            response_json = request.json()
+            self.latest_version = response_json["info"]["version"]
+        else:
+            self.is_invalid = True
 
-        self.latest_version = response_json["info"]["version"]
-        click.echo("Package: %s \n Current: %s Latest: %s\n" % (self.name, self.current_version, self.latest_version))
+    def output_details(self):
+        if not self.is_invalid:
+            click.echo("\nPackage: %s \n Current: %s Latest: %s" % (self.name, self.current_version, self.latest_version))
+        else:
+            click.secho("\nSkipping non PyPI requirement...\n", fg='blue')
--- pypi_uptodate/requirements.py
+++ pypi_uptodate/requirements.py
@@ -2,18 +2,33 @@ import click
 from requirement import Requirement
 
 
-class Requirements:
+class Requirements(object):
 
     def __init__(self, requirements_file):
         self.file = self.read_file(requirements_file)
+        self.requirements = []
 
     def read_file(self, requirements_file):
         click.secho("\nAttempting to read %s ...\n" % requirements_file, fg='magenta')
 
         try:
-            requirements = open(requirements_file).read().splitlines()
-            for line in requirements:
-                Requirement(line)
-
+            requirements_file = open(requirements_file).read().splitlines()
+            with click.progressbar(
+                    iterable=requirements_file,
+                    label='Getting requirements details',
+                    bar_template='%(label)s %(bar)s | %(info)s',
+                    fill_char=click.style(u'█', fg='cyan'),
+                    empty_char=' ') as bar:
+                requirements = []
+                for line in bar:
+                    requirement = Requirement(line)
+                    requirements.append(requirement)
+                self.requirements = requirements
+                self.show_details()
         except OSError:
             return click.secho("Could not find %s. No such file or directory." % (requirements_file), fg='red')
+
+    def show_details(self):
+        click.echo("")
+        for requirement in self.requirements:
+            requirement.output_details()
