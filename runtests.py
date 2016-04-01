7c0e1d944741aac853ad36b02b0e2eaff6d7f527
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-04-02 00:51:23 +0100

    Use nose, remove python 3.5 from tox
    See https://github.com/travis-ci/travis-ci/issues/4794 for more info

--- runtests.py
+++ runtests.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 FLAKE8_ARGS = ['pypiup/', 'tests/', '--ignore=E501']
-COVERAGE_ARGS = ['--source=pypiup', '--omit=pypiup/__init__.py', 'python -m unittest -v tests/tests.py']
+COVERAGE_ARGS = ['--source=pypiup', '--omit=pypiup/__init__.py', 'nosetests', '-v']
 
 
 def exit_on_failure(command, message=None):
@@ -19,10 +19,26 @@ def flake8_main(args):
     return command
 
 
-def run_tests_coverage(args):
-    command = subprocess.call('coverage run --source=pypiup --omit=pypiup/__init__.py -m unittest tests.tests_cli', shell=True)
-    command = subprocess.call(['coverage', 'report'])
+def run_tests(args):
+    print('Running: coverage run %s' % " ".join(COVERAGE_ARGS))
+    command = subprocess.call(['coverage', 'run'] + args, shell=True)
     return command
 
+
+def run_coverage_report():
+    print("\nCoverage Report:")
+    command = subprocess.call(['coverage', 'report', '-m'])
+    return command
+
+
+def run_coverage_html():
+    print("\nGenerating Coverage HTML.")
+    command = subprocess.call(['coverage', 'html'])
+    print("Coverage HTML generated.")
+    return command
+
+
 exit_on_failure(flake8_main(FLAKE8_ARGS))
-exit_on_failure(run_tests_coverage(COVERAGE_ARGS))
+exit_on_failure(run_tests(COVERAGE_ARGS))
+exit_on_failure(run_coverage_report())
+exit_on_failure(run_coverage_html())
